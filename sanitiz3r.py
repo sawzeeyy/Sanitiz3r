b4850749f00f38d0272301fc76204f4119924fb0
Shuaib Oladigbolu <saoladigbolu@gmail.com>
2020-10-28 14:48:59 +0100

    Fix linting errors

--- sanitiz3r.py
+++ sanitiz3r.py
@@ -18,17 +18,18 @@ if sys.version < '3':
     sys.exit()
 
 # OS Compatibility : Coloring
+R, B, Y, C, W = [
+    '\033[1;31m', '\033[1;37m', '\033[93m', '\033[1;30m', '\033[0m']
 if sys.platform.startswith('win'):
-    R, B, Y, C, W = '\033[1;31m', '\033[1;37m', '\033[93m', '\033[1;30m', '\033[0m'
     try:
-        import win_unicode_console, colorama
+        import win_unicode_console
+        import colorama
         win_unicode_console.enable()
         colorama.init()
-    except:
+    except ImportError:
         print('[+] Error: Coloring libraries not installed')
         R, B, Y, C, W = '', '', '', '', ''
-else:
-    R, B, Y, C, W = '\033[1;31m', '\033[1;37m', '\033[93m', '\033[1;30m', '\033[0m'
+
 
 def header():
     print('''%s
@@ -39,29 +40,68 @@ def header():
          /(__/ \_/|_/   |  |_/ |_/  |_/ |_/  /_/  \___/    |_/ v1.0
                                               /|
          %sBy Shuaib Oladigbolu - @_sawzeeyy%s    \|%s          #F007573P %s
-        '''%(R, B, R, C, W))
+        ''' % (R, B, R, C, W))
+
 
 def parse_error(errormsg):
     if len(sys.argv[0]) < 7:
-        print('{}Usage: {} [Options] use -h for help'.format(Y, sys.argv[0]))
+        print(
+            '{}Usage: {} [Options] use -h for help\
+            '.format(Y, sys.argv[0]))
     else:
-        print('{}Usage: {} [Options] use -h for help'.format(Y, sys.argv[0].split('/')[-1]))
+        print(
+            '{}Usage: {} [Options] use -h for help\
+            '.format(Y, sys.argv[0].split('/')[-1]))
+
     print('{}Error: {}{}'.format(R, errormsg, W))
     sys.exit()
 
+
 def parse_args():
     if sys.argv[0][0:2] == './':
-        parser = argparse.ArgumentParser(description='Example: {} -v -a -d domain.com -i file.txt -o custom.html'.format(sys.argv[0]))
+        parser = argparse.ArgumentParser(
+            description='Example: {} -v -a -d domain.com -i\
+                 file.txt -o custom.html'.format(sys.argv[0]))
     else:
-        parser = argparse.ArgumentParser(description='Example: python {} -v -a -d domain.com -i file.txt -o custom.html'.format(sys.argv[0]))
+        parser = argparse.ArgumentParser(
+            description='Example: python {} -v -a -d domain.com\
+                 -i file.txt -o custom.html'.format(sys.argv[0]))
+
     parser.error = parse_error
     parser._optionals.title = "OPTIONS"
-    parser.add_argument('-v', '--verbose', help='Optionally log all information about the current process', required=False, action = 'store_true')
-    parser.add_argument('-a', '--active', help='Optionally choose to take Sanitiz3r online to detemine the validity of subdomains', required=False, action = 'store_true')
-    parser.add_argument('-d', '--domain', help='Specify a domain, a comma separated list or a file containing a list of domains to return the corresponding matching subdomains', required=True)
-    parser.add_argument('-i', '--input', help='Specify an input file or a comma-separated list of the files to sanitize', required=True)
-    parser.add_argument('-o', '--output', help='Optionally specify the filename to save the report', required=False)
+    parser.add_argument(
+        '-v', '--verbose',
+        help='Optionally log all information about the current process',
+        required=False,
+        action='store_true'
+    )
+    parser.add_argument(
+        '-a', '--active',
+        help='Optionally choose to take Sanitiz3r online to detemine\
+             the validity of subdomains',
+        required=False,
+        action='store_true'
+    )
+    parser.add_argument(
+        '-d', '--domain',
+        help='Specify a domain, a comma-separated list or a file\
+             containing a list of domains to return the corresponding\
+             matching subdomains',
+        required=True
+    )
+    parser.add_argument(
+        '-i', '--input',
+        help='Specify an input file or a comma-separated list\
+             of the files to sanitize',
+        required=True)
+    parser.add_argument(
+        '-o', '--output',
+        help='Optionally specify the filename to save the report',
+        required=False
+    )
+
     return parser.parse_args()
+
 
 def base_url(urls_list):
     urls = []
@@ -73,16 +113,17 @@ def base_url(urls_list):
             urls.append(url)
     return urls
 
-def get_urls(domain,files):
+
+def get_urls(domain, files):
     url_list, urls = [], []
     for file in files:
         try:
             lines = open(file)
             line = [url.strip() for url in lines]
             line = base_url(line)
-            line = [extract_url(domain,url) for url in line]
+            line = [extract_url(domain, url) for url in line]
             urls += line
-        except:
+        except IOError:
             print('{}Cannot find : {}{}'.format(R, file, C))
 
     if urls != []:
@@ -96,11 +137,13 @@ def get_urls(domain,files):
         print('{}Exiting since no file(s) found!{}'.format(R, C))
         return('empty')
 
-def extract_url(domains,line):
+
+def extract_url(domains, line):
     urls = []
     domain = [i.split('.')[-1] for i in domains]
     sawzeeyy = '|'.join(domain)
-    url_regex = r"""(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw|""" + sawzeeyy + r""")/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw|"""+ sawzeeyy + r""")\b/?(?!@)))"""
+    url_regex = r"""(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw|""" + sawzeeyy + r""")/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw|""" + sawzeeyy + r""")\b/?(?!@)))"""
+
     urls_found = re.findall(url_regex, line)
     for url in urls_found:
         for domain in domains:
@@ -108,6 +151,7 @@ def extract_url(domains,line):
                 urls.append(url)
     urls = ','.join(urls)
     return urls
+
 
 def get_status(urls, time_out, args):
     print('{}[+] Now checking the individual subdomains{}'.format(Y, C))
@@ -125,20 +169,28 @@ def get_status(urls, time_out, args):
         status[u_status] = temp_status
     return status
 
+
 def get_url_status(url, timeout, arguments):
-    if (arguments.verbose): print('{}[-] Checking {}{}{}'.format(C, R, url, C))
+    if (arguments.verbose):
+        print('{}[-] Checking {}{}{}'.format(C, R, url, C))
+
     full_http_status = get_http_status(url, timeout)
     full_https_status = get_https_status(url, timeout)
     http_url, http_status, http_title = full_http_status
     https_url, https_status, https_title = full_https_status
     url_status = min(http_status, https_status)
     status = 'Not Reachable' if url_status == 1909 else url_status
-    if (arguments.verbose): print('\t{}[+] Status: {}{}'.format(Y, status, C))
+
+    if (arguments.verbose):
+        print('\t{}[+] Status: {}{}'.format(Y, status, C))
+
     if url_status == http_status:
-        if (arguments.verbose): print('\t{}[+] Title: {}{}'.format(Y, http_title, C))
+        if (arguments.verbose):
+            print('\t{}[+] Title: {}{}'.format(Y, http_title, C))
         return [url_status, http_url, http_title]
     else:
-        if (arguments.verbose): print('\t{}[+] Title: {}{}'.format(Y, https_title, C))
+        if (arguments.verbose):
+            print('\t{}[+] Title: {}{}'.format(Y, https_title, C))
         return [url_status, https_url, https_title]
 
 
@@ -147,23 +199,26 @@ def extract_title(html_body):
     title = match.group(1) if match else 'Sorry, couldn\'t wait to check ;)'
     return title
 
-def get_http_status(url, time_out = 10):
+
+def get_http_status(url, time_out=10):
     try:
-        resp = http.client.HTTPConnection(url, timeout = time_out)
+        resp = http.client.HTTPConnection(url, timeout=time_out)
         resp.request('GET', '/')
         response = resp.getresponse()
         title = extract_title(response.read().encode("utf-8"))
         return ['http://' + url, response.status, title]
-    except:
+    except http.client.HTTPException:
         return ['http://' + url, 1909, 'Unidentified']
 
-def get_https_status(url, time_out = 10):
+
+def get_https_status(url, time_out=10):
     try:
-        response = requests.get('https://'+url, timeout = time_out)
+        response = requests.get('https://'+url, timeout=time_out)
         title = extract_title(response.text.encode("utf-8"))
         return ['https://' + url, response.status_code, title]
-    except:
+    except requests.ConnectionError:
         return ['https://' + url, 1909, 'Unidentified']
+
 
 def generate_html_code(*args):
     mode, domain, domain_length = args[0], args[1], args[2]
@@ -181,7 +236,7 @@ def generate_html_code(*args):
       [Subdomain: '<span style='background-color:yellow'>{}</span>']
     </div>
 </div>
-        """.format(url,url,url,url,url)
+        """.format(url, url, url, url, url)
             div_code += part_div_code
 
     elif mode == 'active':
@@ -199,7 +254,9 @@ def generate_html_code(*args):
                   [Subdomain: '<span style='background-color:yellow'>{}</span>'], [Status: '<span style='background-color:yellow'>{}</span>'], [Title: '<span style='background-color:yellow'>{}</span>']
                 </div>
             </div>
-                    """.format(url[0], url[0], url[0].split('/')[-1], status, url[1])
+                    """.format(
+                        url[0], url[0], url[0].split('/')[-1], status, url[1]
+                    )
                 div_code += part_div_code
 
     style = """\
@@ -271,29 +328,39 @@ h1 {
     html_code.append(part_code)
     return html_code, text_format
 
+
 def save_html_file(code, filename):
     fh = open(filename + '.html', 'w')
     txt = open(filename + '.txt', 'w')
-    for st in code[0]: fh.write(st)
-    for text in code[1]: txt.write(text+'\n')
+    for st in code[0]:
+        fh.write(st)
+
+    for text in code[1]:
+        txt.write(text+'\n')
+
     fh.close()
+
 
 def sanitiz3r():
     header()
     args = parse_args()
     domain = args.domain.split(',')
-    filename = domain[0].split('.')[0] + '_sanitiz3r' if type(args.output) != type('F007573P') else args.output.split('.')[0] + '_sanitiz3r'
+
+    if isinstance(args.output, str):
+        filename = domain[0].split('.')[0] + '_sanitiz3r'
+    else:
+        filename = args.output.split('.')[0] + '_sanitiz3r'
 
     if len(domain) == 1 and domain[0].split('.')[1] == 'txt':
         try:
             domain = open(domain[0]).readlines()
-        except:
+        except IOError:
             print('{}Cannot find : {}{}'.format(R, domain[0], C))
             sys.exit()
 
     domain = [i.strip() for i in domain]
     file = args.input.split(',')
-    urls = get_urls(domain,file)
+    urls = get_urls(domain, file)
     filename = os.path.dirname(os.path.abspath(file[0])) + '/' + filename
     mode = 'active' if args.active else 'passive'
 
@@ -315,6 +382,9 @@ def sanitiz3r():
     print('{}[+] Sanitiz3r Operation Completed!{}'.format(Y, W))
     try:
         webbrowser.open_new_tab('file:///' + filename + '.html')
-    except:
+    except IOError:
         webbrowser.open_new_tab(filename + '.html')
-if __name__ == '__main__': sanitiz3r()
+
+
+if __name__ == '__main__':
+    sanitiz3r()
